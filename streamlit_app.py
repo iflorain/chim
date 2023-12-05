efb6cfa4783b5f3a591881af579d4ae6ac4067fc
iflorain <150113291+iflorain@users.noreply.github.com>
2023-12-05 20:20:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,46 @@
-
+# Import from 3rd party libraries
 import streamlit as st
 import openai
-import pandas as pd
 
-# Uncomment the following lines to enable the API key input form
-# Initialize
-st.cache_data.clear()
+#Init
+openai_api_key = None
+api_key_form_submitted = False
+#openai.api_key = ""
 
-if "openai_api_key" not in st.session_state:
-    st.session_state.openai_api_key = ""
 
-openai.api_key = st.session_state.openai_api_key
+# Define functions
 
-if "text_error" not in st.session_state:
-    st.session_state.text_error = None
+def analyse_text(text_input: str, strictness: str):
+    if not text_input:
+        st.session_state.text_error = "Please enter your text"
+        return
 
-if "text" not in st.session_state:
-    st.session_state.text = None
+    with text_spinner_placeholder:
+        with st.spinner("Please wait while your text is analysed..."):
+            prompt = text_input
+            st.session_state.n_requests += 1
+            st.session_state.text = (
+                openai.chat.completions.create(
+                model="gpt-3.5-turbo",
+                temperature=0.4,
+                top_p = 0.8,
+                max_tokens = 450,
+                messages=[
+                    {"role": "system", "content": f"You are a grammar checker bot that outputs in formatted html. Check the following text for grammar mistakes in the context of {strictness} Correct the errors and provide a list of (Fairly simple) explanations."},
+                    {"role": "user", "content": f"Please fix this, highlighting any changes in green and, more importantly, showing the original with mistakes in red: {prompt}"},
+                    ]
+                )
+            )
+
+
+
+# Render Streamlit page
+st.set_page_config(page_title="GrammarBuddy", page_icon="🤖")
+st.session_state.text_error = None
+st.session_state.text = None
+
+
+text_spinner_placeholder = st.spinner()
 
 if "n_requests" not in st.session_state:
     st.session_state.n_requests = 0
@@ -27,54 +51,40 @@ with st.sidebar:
     api_key_form_submitted = api_key_form.form_submit_button("Submit")
 
     if api_key_form_submitted:
-        st.session_state.openai_api_key = openai_api_key
-        openai.api_key = st.session_state.openai_api_key
-        st.success("Your OpenAI API key was saved successfully!")
+        if True:                              #check_openai_api_key(openai_api_key):
+            openai.api_key = openai_api_key
+            st.success("Your OpenAI API key was saved successfully!")
+        else:
+            st.info("Your OpenAI API key is invalid, please check to see if it is correctly inputted or contact OpenAI")
 
-def generate_flower_recommendation(occasion, recipient_name, favorite_color, relationship):
-    # Customize the prompt based on your requirements
-    #prompt1 = f"Recommend me a flower name that are suitable for {occasion} and {favorite_color} and {relationship} for {recipient_name} who is my {relationship}."
-    #prompt2 = f'write 5 different notes for me to tell {recipient_name} who is my {relationship} why I chose this flower for this {occasion}.'
-    prompt = f'Give me 2 recommended flowers that are suitable for {occasion} and {favorite_color} and {relationship} for {recipient_name} who is my {relationship}.and for each flowers write 3 different notes for me to tell {recipient_name} who is my {relationship} why I chose this flower for this {occasion}.'
-    # Call OpenAI API for recommendation
-    response = openai.chat.completions.create(
-        model="gpt-3.5-turbo",
-        temperature=0.8,
-        top_p=0.1,
-        max_tokens=450,
-        messages=[
-            {"role": "system", "content": f"You are a flowers recommendation bot. You will help users find the best flowers for their important person from the context{occasion} and {favorite_color} and {relationship}"},
-            {"role": "user", "content": f"You will help users find 2 flowers and make 3 different notes for each flowers from the context:{prompt}"},
-        ]
-    )
+
+
+text_input_form = st.form(key = "text_input_form")
+text_input = text_input_form.text_area(label="Input your text here", placeholder="Mary hadn't a little lamb.")
+strictness = text_input_form.text_area(label="How strict should the checker be?", placeholder="(e.g. academic (default), slang, business)", height=50)
+text_input_form_submitted = text_input_form.form_submit_button("Submit")
+
+
+if not strictness:
+    strictness = "academic"
+if not text_input:
+    st.info("Please add some text to continue.")
+else:     
+    analyse_text(text_input, strictness)
     
-    return response.choices[0].message.content
+    
 
-# Center the title
-st.markdown("<div style='text-align: center;'><h2 style='font-size: 2rem;'>🌼Flower For Your Important Person🌼</h2></div>", unsafe_allow_html=True)
+text_spinner_placeholder = st.empty()
 
-# Uncomment the following lines to enable the API key input form
+if st.session_state.text_error:
+    st.error(st.session_state.text_error)
+
+if st.session_state.text:
+    st.markdown("""---""")
+    output = st.session_state.text
+    st.markdown(f"<h3>Here's what your GrammarBuddy has to say: </h3><p>\t{output.choices[0].message.content}</p>", unsafe_allow_html=True)
 
 
-# User input
-occasion = st.text_input("Occasion:")
-recipient_name = st.text_input("Recipient's Name:")
-favorite_color = st.text_input("Recipient's Favorite Color:")
-relationship = st.text_input("Recipient's Relationship to you:")
-
-# Generate recommendation
-if st.button("Generate Recommendation"):
-    if occasion and recipient_name and favorite_color and relationship:
-        recommendation = generate_flower_recommendation(
-            occasion, recipient_name, favorite_color, relationship
-        )
-        st.success(f"{recommendation}")
-    else:
-        st.warning("Please fill in all fields.")
-        
-
-# Center the title
-st.markdown("<div style='text-align: center;'><h2 style='font-size: 1.5rem;'><i>“I must have flowers, always, and always.”</i></h2></div>", unsafe_allow_html=True)
-# Center the title
-st.markdown("<div style='text-align: center;'><h2 style='font-size: 1rem;'><i>— Claude Monet —</i></h2></div>", unsafe_allow_html=True)
-
+    # st.write(f":red[{text_input}]")
+    # st.text_area(label="Corrected", value=f"{output.choices[0].message.content}", height=400)
+    image_spinner_placeholder = st.empty()
